b63ac53515f7c8e9282e6c3ccd9a464097844b83
waemm <151449373+waemsiey@users.noreply.github.com>
2025-06-12 16:45:30 +0800

    changes main method to run

--- app/main.py
+++ app/main.py
@@ -86,5 +86,6 @@ async def log_info(request: Request):
 
     return {"message": "Info logged"}
 
-if __name__== "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
